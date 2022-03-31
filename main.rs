661703b2291f75ffa5328aabbf25c0f2cd3b13bb
Albert Portnoy <albert@albertp.dev>
2022-03-30 19:39:39 -0500

    More performance improvements

--- main.rs
+++ main.rs
@@ -1,7 +1,7 @@
 use crossterm::{
     cursor::MoveToColumn, execute, style::Print, terminal::Clear, terminal::ClearType,
 };
-use fastrand;
+use fastrand::{self, Rng};
 use num_format::{Locale, ToFormattedString};
 use std::{
     env,
@@ -76,23 +76,23 @@ Never gonna tell a lie and hurt you
 ";
 
 fn main() {
-    println!("");
+    println!();
 
     let num_threads = env::var("THREADS")
-        .unwrap_or("2".to_string())
-        .parse::<i32>()
+        .unwrap_or_else(|_| String::from("2"))
+        .parse::<u16>()
         .unwrap();
 
-    let mut attempts: u128 = 0;
-    let mut best_length: i32 = 0;
+    let mut attempts: u64 = 0;
+    let mut best_length: u16 = 0;
 
     let start_time = Instant::now();
 
-    let mut progress_vec: Vec<(Instant, u128)> = vec![(start_time, 0)];
+    let mut progress_vec: Vec<(Instant, u64)> = vec![(start_time, 0)];
 
     // For threads sending their results to the main thread
     // Touple of (last_index, saved_attempts, thread_index)
-    let (tx, rx) = sync_channel(64) as (SyncSender<(i32, u128, i32)>, Receiver<(i32, u128, i32)>);
+    let (tx, rx) = sync_channel(64) as (SyncSender<(u16, u64, u16)>, Receiver<(u16, u64, u16)>);
     // Spawn threads
     for i in 0..num_threads {
         // Clone transmitter so I can put it in the thread
@@ -100,16 +100,18 @@ fn main() {
 
         thread::spawn(move || {
             // Save the number of attempts that need to be sent to the main thread
-            let mut saved_attempts: u128 = 0;
+            let mut saved_attempts: u64 = 0;
 
             let chars = gen_char_vec();
 
+            let rng = fastrand::Rng::new();
+
             loop {
-                let last_index = run_attempt(&chars);
+                let last_index = run_attempt(&chars, &rng);
                 saved_attempts += 1;
                 // Check if the thread should send info to the main thread
                 // This should happen if the thread reaches a new best length or every second
-                if last_index > best_length || last_index == -1 || saved_attempts > 1000000 {
+                if saved_attempts > 1000000 || last_index > best_length {
                     if last_index > best_length {
                         // Update saved best length
                         best_length = last_index;
@@ -126,107 +128,105 @@ fn main() {
         println!("Spawned monkey #{}", i + 1);
     }
 
+    let total = num_chars(SCRIPT);
+
     println!(
         "\nAll monkeys spawned! {} monkeys are typing the lyrics of \"Never gonna give you up\" ({} letters)\n",
         num_threads,
-        num_chars(&SCRIPT.to_string()).to_formatted_string(&Locale::en),
+        total.to_formatted_string(&Locale::en),
     );
 
     loop {
         let mut stdout = stdout();
-        match rx.recv() {
-            // Data recieved from a thread
-            Ok((last_index, saved_attempts, i)) => {
-                // Update attempts
-                attempts += saved_attempts;
+        if let Ok((last_index, saved_attempts, i)) = rx.recv() {
+            // Update attempts
+            attempts += saved_attempts;
 
-                // Check if the thread reached the end of the lyrics
-                if last_index == -1 {
-                    // The monkey did it!
-                    execute!(stdout, Clear(ClearType::CurrentLine), MoveToColumn(1), Print(format!(
-						"MONKEY #{} DID IT! All {} letters of the lyrics of \"Never gonna give you up\" were correctly typed. This took {} attempts ({}).",
-						i+1,
-						num_chars(&SCRIPT.to_string()).to_formatted_string(&Locale::en),
-						attempts.to_formatted_string(&Locale::en),
-						duration_string(start_time))
-					)).ok();
-                    exit(0);
-                }
+            // Check if the thread reached the end of the lyrics
+            if last_index == total {
+                // The monkey did it!
+                execute!(stdout, Clear(ClearType::CurrentLine), MoveToColumn(1), Print(format!(
+        						"MONKEY #{} DID IT! All {} letters of the lyrics of \"Never gonna give you up\" were correctly typed. This took {} attempts ({}).",
+        						i+1,
+        						num_chars(SCRIPT).to_formatted_string(&Locale::en),
+        						attempts.to_formatted_string(&Locale::en),
+        						duration_string(start_time))
+        					)).ok();
+                exit(0);
+            }
 
-                // Check if the thread reached a new best length
-                if last_index > best_length {
-                    // New best
-                    let text = SCRIPT.chars().take(last_index as usize).collect::<String>();
-                    execute!(
-                        stdout,
-                        Clear(ClearType::CurrentLine),
-                        MoveToColumn(1),
-                        Print(format!(
-                            "Monkey #{} got a new best of {} letter(s) on attempt {} ({}):\n{}\n\n",
-                            i + 1,
-                            num_chars(&text).to_formatted_string(&Locale::en),
-                            attempts.to_formatted_string(&Locale::en),
-                            duration_string(start_time),
-                            text
-                        )),
-                    )
-                    .ok();
-                    best_length = last_index;
-                }
-
-                // Remove elements older than 5 minutes
-                progress_vec.retain(|&(time, _)| time.elapsed().as_secs() < 60 * 1);
-
-                if progress_vec.last().unwrap().0.elapsed().as_secs() >= 1 {
-                    // Record new progress
-                    progress_vec.push((Instant::now(), attempts));
-                }
-
-                let first_progress = progress_vec.first().unwrap();
-
-                let seconds_elaped = first_progress.0.elapsed().as_secs() as u128;
-                let attempts_elapsed = attempts - first_progress.1;
-                let attemps_per_second = if seconds_elaped > 0 {
-                    attempts_elapsed / seconds_elaped
-                } else {
-                    0
-                };
-
+            // Check if the thread reached a new best length
+            if last_index > best_length {
+                // New best
+                let text = SCRIPT.chars().take(last_index as usize).collect::<String>();
                 execute!(
                     stdout,
                     Clear(ClearType::CurrentLine),
                     MoveToColumn(1),
                     Print(format!(
-                        "Ran {} attempts in {} ({}/s)",
-                        sig_figs(attempts, 3, 11).to_formatted_string(&Locale::en),
+                        "Monkey #{} got a new best of {} letter(s) on attempt {} ({}):\n{}\n\n",
+                        i + 1,
+                        num_chars(&text).to_formatted_string(&Locale::en),
+                        attempts.to_formatted_string(&Locale::en),
                         duration_string(start_time),
-                        sig_figs(attemps_per_second, 3, 0).to_formatted_string(&Locale::en),
-                    ))
+                        text
+                    )),
                 )
                 .ok();
+                best_length = last_index;
             }
-            Err(_) => (),
+
+            // Remove elements older than 5 minutes
+            progress_vec.retain(|&(time, _)| time.elapsed().as_secs() < 60);
+
+            if progress_vec.last().unwrap().0.elapsed().as_secs() >= 1 {
+                // Record new progress
+                progress_vec.push((Instant::now(), attempts));
+            }
+
+            let first_progress = progress_vec.first().unwrap();
+
+            let seconds_elaped = first_progress.0.elapsed().as_secs() as u64;
+            let attempts_elapsed = attempts - first_progress.1;
+            let attemps_per_second = if seconds_elaped > 0 {
+                attempts_elapsed / seconds_elaped
+            } else {
+                0
+            };
+
+            execute!(
+                stdout,
+                Clear(ClearType::CurrentLine),
+                MoveToColumn(1),
+                Print(format!(
+                    "Ran {} attempts in {} ({}/s)",
+                    sig_figs(attempts, 3, 11).to_formatted_string(&Locale::en),
+                    duration_string(start_time),
+                    sig_figs(attemps_per_second, 3, 0).to_formatted_string(&Locale::en),
+                ))
+            )
+            .ok();
         };
     }
 }
 
 /// Runs an attempt at typing the lyrics
 /// Returns the amount of characters correctly typed
-fn run_attempt(chars: &Vec<char>) -> i32 {
-    let mut last_index: i32 = -1;
+fn run_attempt(chars: &[char], rng: &Rng) -> u16 {
+    let mut last_index: u16 = 0;
     for char in chars {
-        last_index += 1;
-        if choose_character().ne(&char) {
-            return last_index;
+        if choose_character(rng).ne(char) {
+            break;
         }
+        last_index += 1;
     }
 
-    -1
+    last_index
 }
 
 /// Choose a random character from CHARACTERS
-fn choose_character() -> char {
-    CHARACTERS[fastrand::usize(0..CHARACTERS.len())]
+fn choose_character(rng: &Rng) -> char {
+    CHARACTERS[rng.usize(0..CHARACTERS.len())]
 }
 /// Generate duration string
 fn duration_string(start_time: Instant) -> String {
@@ -240,7 +240,7 @@ fn duration_string(start_time: Instant) -> String {
 }
 
 /// Calculate number of characters that are in the character set
-fn num_chars(s: &String) -> i32 {
+fn num_chars(s: &str) -> u16 {
     let mut i = 0;
     for c in s.to_lowercase().chars() {
         if CHARACTERS.contains(&c) {
@@ -254,7 +254,7 @@ fn num_chars(s: &String) -> i32 {
 fn gen_char_vec() -> Vec<char> {
     let mut chars = Vec::from_iter(SCRIPT.to_lowercase().chars());
     for (i, char) in chars.clone().iter().enumerate().rev() {
-        if !CHARACTERS.contains(&char) {
+        if !CHARACTERS.contains(char) {
             chars.remove(i);
         }
     }
@@ -262,13 +262,13 @@ fn gen_char_vec() -> Vec<char> {
 }
 
 /// Round number to significant figures
-fn sig_figs<T: Into<u128>>(number: T, sig_figs: u32, max_digits: u32) -> u128 {
-    let number: u128 = number.into();
+fn sig_figs<T: Into<u64>>(number: T, sig_figs: u32, max_digits: u32) -> u64 {
+    let number: u64 = number.into();
     let mut digits = number.to_string().len() as u32;
     if max_digits != 0 && digits > max_digits {
         digits = max_digits;
     }
-    let divide_by = 10u128.pow(digits - sig_figs);
+    let divide_by = 10u64.pow(digits - sig_figs);
     if divide_by == 0 {
         return number;
     }
